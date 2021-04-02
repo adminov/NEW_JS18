9fe53d0c1aa43f461b09aabfbb9a2255263dba52
adminov <batishnazik@mail.com>
2021-04-02 19:02:09 +0600

    второй урок

--- mainjs/js.js
+++ mainjs/js.js
@@ -1,10 +1,20 @@
-console.log('Hello, Maxim');
-alert('hello, Виталий Моренко');
-alert('hello, gloAcademy');
+let money = 100000; //Доход за месяц
+let Income = 'фриланс'; //доп. доход
+let addExpenses = 'свет, газ, мусор, бензин, продукты, налог'; // расходы
+let deposit = 50000;
+let mission = 70000;// Какую сумму хотите накопить
+let period  = 12; //месяцев
 
-let money,
-    income,
-    addExpenses,
-    deposit,
-    mission,
-    period;
+let yearMoney = deposit * period;
+
+console.log("Доход за месяц: " + money, "\nдоп. доход: " + Income, "\ndeposit: " + deposit);
+
+console.log("\nрасходы: " + addExpenses.length);
+
+console.log("Период и Цель заработать в год: " + period + ' * ' +  mission + ' = ' + yearMoney);
+
+console.log(addExpenses.toUpperCase() + "\n");
+console.log(addExpenses.split(', '));
+
+let budgetDay = 80000; //доход за месяц / 30
+console.log(budgetDay);
