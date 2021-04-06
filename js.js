0e1ad8a5b69b813232376b125e5e9b3a5811f46c
adminov <batishnazik@gmail.com>
2021-04-07 01:02:09 +0600

    3 урок усложнение

---
+++ js.js
@@ -0,0 +1,47 @@
+'use strict';
+let lang = prompt('Напишите: ru или en');
+
+//через многомерный массив:
+
+let lan = {
+    'ru': ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'],
+    'en': ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn']
+};
+
+console.log(lan[lang]);
+
+//через ив
+if (lang === 'ru') {
+    lang = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
+} else {
+    lang = ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'];
+}
+
+console.log(lang);
+
+//через свитч
+switch (lang) {
+    case 'ru':
+        lang = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
+        break;
+    case 'en':
+        lang = ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'];
+}
+
+console.log(lang);
+
+
+
+let namePerson = {
+    Jek : 'директор',
+    Maxim : 'преподаватель',
+    Noname : 'студент',
+};
+
+let result = namePerson.Jek ? 'директор' : 'error';
+let result1 = namePerson.Maxim ? 'директор' : 'error';
+let result2 = namePerson.Noname ? 'директор' : 'error';
+
+console.log(result);
+console.log(result1);
+console.log(result2);
