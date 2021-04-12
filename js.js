91c0520e0f50503aab16ce9f958ecd2b29cea258
Babur <batishnazik@gmail.com>
2021-04-13 01:01:52 +0600

    Update js.js

--- js.js
+++ js.js
@@ -1,38 +1,29 @@
 'use strict';
-let lang = prompt('Напишите: ru или en');
-{
-//через многомерный массив:
-    let lan = {
-        'ru': ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'],
-        'en': ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn']
-    };
-    console.log(lan[lang]);
-}
+let lang = 'en';
 
-{
 //через ив
     if (lang === 'ru') {
-        lang = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
+        console.log('пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс');
     } else {
-        lang = ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'];
+        console.log('mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn');
     }
 
-    console.log(lang);
-}
-
-{
 //через свитч
     switch (lang) {
         case 'ru':
-            lang = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
+            console.log('пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс');
             break;
         case 'en':
-            lang = ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'];
+            console.log('mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn');
         default:
     }
 
-    console.log(lang);
-}
+//через многомерный массив:
+let lan = {
+    'ru': ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'],
+    'en': ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn']
+};
+console.log(lan[lang]);
 
 
 let namePerson = 'Maxim';
