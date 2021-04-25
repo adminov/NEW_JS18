536d4dc021cfe6635543a97d5a0411a3fd8178a1
adminov <batishnazik@gmail.com>
2021-04-26 00:50:22 +0600

    теперь точно проверил все

--- index.js
+++ index.js
@@ -14,22 +14,45 @@ let	additionalIncome = document.getElementsByClassName('additional_income-value'
 let	additionalExpenses = document.getElementsByClassName('additional_expenses-value')[0];
 let	incomePeriod = document.getElementsByClassName('income_period-value')[0];
 let	targetMonths = document.getElementsByClassName('target_month-value')[0];
+
+console.log(budgetMonth);
+console.log(budgetDay);
+console.log(expensesMonth);
+console.log(additionalIncome);
+console.log(additionalExpenses);
+console.log(incomePeriod);
+console.log(targetMonths);
+console.log('------------------------------------------------------');
 //---------------------------------------------------------------------------------------
 let salaryAmount = document.querySelector('.salary-amount'),
 	incomeTitle = document.querySelector('.income-title'),
 	incomeAmount = document.querySelector('.income-amount'),
 	expensesTitle = document.querySelector('.expenses-title'),
-	expensesAmount = document.querySelector('expenses-amount'),
+	expensesAmount = document.querySelector('.expenses-amount'),
 	additionalExpensesItem = document.querySelector('.additional_expenses-item'),
 	targetAmount = document.querySelector('.target-amount'),
 	periodSelect = document.querySelector('.period-select');
-//---------------------------------------------------------------------------------------
-let Calculate = document.getElementById('start');
-let depositCheck = document.querySelector('#deposit-check');
-let btnPlus = document.getElementsByTagName('btn_plus');
-let btnPlusTwo = document.getElementsByTagName('btn_plus');
-let additionalIncomeItem = document.querySelectorAll('.additional_income-item');
 
+console.log(salaryAmount);
+console.log(incomeTitle);
+console.log(incomeAmount);
+console.log(expensesTitle);
+console.log(expensesAmount);
+console.log(additionalExpensesItem);
+console.log(targetAmount);
+console.log(periodSelect);
+console.log('------------------------------------------------------');
+//---------------------------------------------------------------------------------------
+let сalculate = document.getElementById('start');
+let depositCheck = document.querySelector('#deposit-check');
+let btnPlus = document.getElementsByTagName('button')[0];
+let btnPlusTwo = document.getElementsByTagName('button')[1];
+let additionalIncomeItem = document.querySelectorAll('.additional_income-item');
+console.log(сalculate);
+console.log(depositCheck);
+console.log(btnPlus);
+console.log(btnPlusTwo);
+console.log(additionalIncomeItem);
 //----------------------------------------------------------------------------
 let money,
 	start = () => {
