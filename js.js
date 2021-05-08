e6b7e9793f2fe00bee74f32da7ed576a38f74616
adminov <batishnazik@gmail.com>
2021-05-08 14:34:46 +0600

    lesson14_part_1

--- js.js
+++ js.js
@@ -1,18 +1,49 @@
 'use strict';
 
-const hardTask = (arg) => {
-        let res = '';
-    if (typeof arg === 'string'){
-        const lengthMax = 30;
-        const str = arg.trim();
-        if (str.length > lengthMax){
-            res = str.substr(0, lengthMax) + '...';
-        } else {
-            res = str;
-        }
+function DomElement(selector, height, width, bg, fontSize) {
+    this.selector = selector;
+    this.width = width;
+    this.height = height;
+    this.bg = bg;
+    this.fontSize = fontSize;
+}
+
+DomElement.prototype.start = function(){
+    let div;
+    if (this.selector.substr(0, 1) === '.'){
+      div = document.createElement('div');
+      div.className = 'selector';
+  } else if (this.selector.substr(0, 1) === '#') {
+        div = document.createElement('p');
+        div.className = 'selector';
     }
-    return res;
+
+    div.style.cssText = `
+        width: ${this.width}px;
+        height: ${this.height}px;
+        background: ${this.bg};
+        font-size: ${this.fontSize}px;`;
+    document.body.append(div);
+
+    let text = document.querySelector('.selector');
+    text.textContent  = 'hello everyone';
 };
-console.log(hardTask(5));
-console.log(hardTask(' my names is Batish '));
-console.log(hardTask(' my names is Batish, he lives in Kyrgyzstan '));
+
+let newDomElem = new DomElement('.bar', 100, 100, 'red', 35);
+
+newDomElem.start();
+
+//
+// Car.prototype.ride = function(){
+//   console.log(this.brand + ' ' +  this.model + ' lets go');
+// };
+//
+// function Audi (country, options, model, type) {
+//     Car.apply(this, arguments);
+//     this.brand = 'Audi';
+//     this.model = model;
+//     this.type = type;
+// }
+//
+// Audi.prototype = Object.create(Car.prototype);
+// Audi.prototype.constructor = Audi;
