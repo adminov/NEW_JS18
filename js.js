9999bde56651039572e799d078e0df23a703b03f
adminov <batishnazik@gmail.com>
2021-05-18 01:02:48 +0600

    edited 3

--- js.js
+++ js.js
@@ -1,42 +1,64 @@
-document.addEventListener('DOMContentLoaded', function (){
+document.addEventListener('DOMContentLoaded', () => {
     'use strict';
-
     //timer
     function countTimer(deadLine) {
-        let timerHour = document.querySelector('#timer-hours'),
+        const timerHour = document.querySelector('#timer-hours'),
             timerMinutes = document.querySelector('#timer-minutes'),
             timerSeconds = document.querySelector('#timer-seconds');
 
         function getTimeRemaining() {
-            let dateStop = new Date(deadLine).getTime(),
+            const dateStop = new Date(deadLine).getTime(),
                 dateNow = new Date().getTime(),
                 timeRemaining = (dateStop - dateNow) / 1000,
                 second = Math.floor(timeRemaining % 60),
                 minutes = Math.floor((timeRemaining / 60) % 60),
                 hours = Math.floor(timeRemaining / 60 / 60);
             //let day = Math.floor(timeRemaining / 60 / 60 /24);
-            return {timeRemaining, hours, minutes, second}
+            return { timeRemaining, hours, minutes, second };
         }
 
         function updateClock() {
-            let timer = getTimeRemaining();
+            const timer = getTimeRemaining();
+            timerHour.textContent = ('0' + timer.hours).slice(-2);
+            timerMinutes.textContent = ('0' + timer.minutes).slice(-2);
+            timerSeconds.textContent = ('0' + timer.second).slice(-2);
 
-                timerHour.textContent = ('0' + timer.hours).slice(-9);
-                timerMinutes.textContent = timer.minutes;
-                timerSeconds.textContent = timer.second;
 
+        }
 
-            if (timer.timeRemaining > 0){
-                setInterval(updateClock, 1000);
-            } else {
-                timerHour.textContent = '00';
-                timerMinutes.textContent = '00';
-                timerSeconds.textContent = '00';
-            }
+        const timer = getTimeRemaining();
+        if (timer.timeRemaining > 0) {
+            setInterval(updateClock, 1000);
+        } else {
+            timerHour.textContent = '00';
+            timerMinutes.textContent = '00';
+            timerSeconds.textContent = '00';
         }
 
         updateClock();
+
+    }
+    countTimer('19 May 2021');
+
+    //меню
+    const toggleMenu = () =>{
+        const btnMenu = document.querySelector('.menu'),
+            menu = document.querySelector('menu'),
+            closeBtn = document.querySelector('close-btn');
+
+        btnMenu.addEventListener('click', () => {
+            if (!menu.style.transform || menu.style.transform === `translate(-100%)`){
+                menu.style.transform = `translate(0)`;
+            } else {
+                menu.style.transform = `translate(-100%)`;
+            }
+
+        });
+
+        closeBtn.addEventListener('click', () => {
+            menu.style.transform = `translate(-100%)`;
+        })
     };
 
-    countTimer('16 May 2021');
+    toggleMenu();
 });
