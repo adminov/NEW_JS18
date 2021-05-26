3cd9ff313ed69c9cca70ab3b6ee051052b41aea8
adminov <batishnazik@gmail.com>
2021-05-26 23:24:54 +0600

    lesson19

--- js.js
+++ js.js
@@ -38,27 +38,67 @@ document.addEventListener('DOMContentLoaded', () => {
         updateClock();
 
     }
-    countTimer('19 May 2021');
+    countTimer('27 May 2021');
 
     //меню
     const toggleMenu = () =>{
         const btnMenu = document.querySelector('.menu'),
             menu = document.querySelector('menu'),
-            closeBtn = document.querySelector('close-btn');
+            closeBtn = document.querySelector('.close-btn'),
+        menuItems = menu.querySelectorAll('ul>li');
 
-        btnMenu.addEventListener('click', () => {
-            if (!menu.style.transform || menu.style.transform === `translate(-100%)`){
-                menu.style.transform = `translate(0)`;
-            } else {
-                menu.style.transform = `translate(-100%)`;
-            }
-
-        });
-
-        closeBtn.addEventListener('click', () => {
-            menu.style.transform = `translate(-100%)`;
+        const handlerMenu = () => {
+            menu.classList.toggle('active-menu');
+        };
+        btnMenu.addEventListener('click', handlerMenu);
+        closeBtn.addEventListener('click', handlerMenu);
+        menuItems.forEach((elem) => {
+            elem.addEventListener('click', handlerMenu)
         })
     };
 
     toggleMenu();
+
+   //popup
+    const togglePopup = () => {
+        const popup = document.querySelector('.popup'),
+            popupBtn = document.querySelectorAll('.popup-btn'),
+            popupClose = document.querySelector('.popup-close'),
+            popupContent = document.querySelector('.popup-content'),
+        popupData = {
+            count: 150,
+            speed: 3,
+            start: 150,
+            end: 0
+        };
+
+        const showPopup = () => {
+            popupData.start > popupData.end ?
+                popupData.count -= popupData.speed :
+                popupData.count += popupData.speed;
+            popupContent.style.transform = `translateX(${popupData.count}px)`;
+
+            if (popupData.start > popupData.end ?
+            popupData.count > popupData.end :
+            popupData.count < popupData.end){
+                requestAnimationFrame(showPopup);
+            }
+        };
+
+        popupBtn.forEach((elem) => {
+           elem.addEventListener('click', () => {
+               popup.style.display = 'block';
+               if (screen.width > 768) {
+                   popupData.count = popupData.start;
+                   requestAnimationFrame(showPopup);
+               }
+           });
+        });
+
+        popupClose.addEventListener('click', () => {
+            popup.style.display = 'none';
+        });
+    };
+
+    togglePopup()
 });
