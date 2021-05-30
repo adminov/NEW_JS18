4241b74b34d9107c0a923bbc92c52635a6131c4e
adminov <batishnazik@gmail.com>
2021-05-31 03:32:24 +0600

    lesson24

--- js.js
+++ js.js
@@ -338,5 +338,63 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
     };
-    checkEmailAll()
+    checkEmailAll();
+
+    //калькулятор
+
+    const calc = (price = 100) => {
+      const calcBlock = document.querySelector('.calc-block'),
+          calcType = document.querySelector('.calc-type'),
+          calcSquare = document.querySelector('.calc-square'),
+          calcDay = document.querySelector('.calc-day'),
+          calcCount = document.querySelector('.calc-count'),
+          totalValue = document.getElementById('total');
+
+      const countSum = () => {
+          let total = 0,
+            countValue = 1,
+            dayValue = 1;
+          let step = 50;
+          const typeValue = calcType.options[calcType.selectedIndex].value,
+              squareValue = +calcSquare.value;
+
+          if (calcCount.value > 1){
+              countValue += (calcCount.value - 1) / 10;
+          }
+          if (calcDay.value && calcDay.value < 5){
+              dayValue *= 2;
+          } else if (calcDay.value && calcDay.value < 10){
+              dayValue *= 1.5;
+          }
+
+          if (typeValue && squareValue){
+              total = price * typeValue * squareValue * countValue * dayValue;
+          }
+
+          if (totalValue.textContent != total) {
+              if (totalValue.textContent > total) {
+                  step = -1;
+              }
+
+              let timer = setInterval(() => {
+                  totalValue.textContent = +totalValue.textContent + step;
+                  if ((total - totalValue.textContent) * step < 1) {
+                      clearInterval(timer);
+                      totalValue.textContent = Math.round(total);
+                  }
+              }, 0);
+          }
+      };
+
+      calcBlock.addEventListener('change', (event) => {
+         const target = event.target;
+
+         if (target.matches('select') || target.matches('input')){
+             countSum();
+         }
+      });
+
+    };
+
+    calc(100);
 });
