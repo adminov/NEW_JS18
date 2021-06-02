3b995c4fe6f05385469f551930ee62623fab8d5d
adminov <batishnazik@gmail.com>
2021-06-03 02:03:46 +0600

    lesson26

--- js.js
+++ js.js
@@ -38,14 +38,14 @@ document.addEventListener('DOMContentLoaded', () => {
         // updateClock();
 
     }
-    countTimer('29 May 2021');
+    countTimer('4 Jun 2021');
 
     //меню
     const toggleMenu = () =>{
         const menu = document.querySelector('menu');
 
         document.addEventListener('click', (event) => {
-           let target = event.target;
+            let target = event.target;
             if (target.closest('.menu')){
                 menu.classList.toggle('active-menu');
             } else if (target.closest('.close-btn') || target.closest('a') || !target.closest('.active-menu')){
@@ -56,17 +56,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
     toggleMenu();
 
-   //popup
+    //popup
     const togglePopup = () => {
         const popup = document.querySelector('.popup'),
             popupBtn = document.querySelectorAll('.popup-btn'),
             popupContent = document.querySelector('.popup-content'),
-        popupData = {
-            count: 150,
-            speed: 3,
-            start: 150,
-            end: 0
-        };
+            popupData = {
+                count: 150,
+                speed: 3,
+                start: 150,
+                end: 0
+            };
 
         const showPopup = () => {
             popupData.start > popupData.end ?
@@ -75,20 +75,20 @@ document.addEventListener('DOMContentLoaded', () => {
             popupContent.style.transform = `translateX(${popupData.count}px)`;
 
             if (popupData.start > popupData.end ?
-            popupData.count > popupData.end :
-            popupData.count < popupData.end){
+                popupData.count > popupData.end :
+                popupData.count < popupData.end){
                 requestAnimationFrame(showPopup);
             }
         };
 
         popupBtn.forEach((elem) => {
-           elem.addEventListener('click', () => {
-               popup.style.display = 'block';
-               if (screen.width > 768) {
-                   popupData.count = popupData.start;
-                   requestAnimationFrame(showPopup);
-               }
-           });
+            elem.addEventListener('click', () => {
+                popup.style.display = 'block';
+                if (screen.width > 768) {
+                    popupData.count = popupData.start;
+                    requestAnimationFrame(showPopup);
+                }
+            });
         });
 
         popup.addEventListener('click', (event) => {
@@ -126,15 +126,15 @@ document.addEventListener('DOMContentLoaded', () => {
         };
 
         tabHeader.addEventListener('click', (event) => {
-           let target = event.target;
-           target = target.closest('.service-header-tab');
-           if (target){
-               tab.forEach((item, index) => {
-                   if (item === target){
-                       toggleTabContent(index)
-                   }
-               });
-           }
+            let target = event.target;
+            target = target.closest('.service-header-tab');
+            if (target){
+                tab.forEach((item, index) => {
+                    if (item === target){
+                        toggleTabContent(index)
+                    }
+                });
+            }
 
         });
 
@@ -147,15 +147,15 @@ document.addEventListener('DOMContentLoaded', () => {
             event.preventDefault();
             const targetId = event.currentTarget.getAttribute('href');
             window.scrollTo({
-               top: document.querySelector(targetId).offsetTop,
+                top: document.querySelector(targetId).offsetTop,
                 behavior: 'smooth'
             });
         };
 
         const mainA = document.querySelectorAll('menu a');
         mainA.forEach((elem) => {
-             elem.addEventListener('click', liClick);
-         });
+            elem.addEventListener('click', liClick);
+        });
     };
     animateScroll();
 
@@ -242,10 +242,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
         // при наведение мышки вызывается ф-я stopSlide
         slider.addEventListener('mouseover', (event) => {
-           const target = event.target;
+            const target = event.target;
             if (target.matches('.portfolio-btn') || target.matches('.dot')){
-               stopSlide()
-           }
+                stopSlide()
+            }
         });
         // при наведение мышки вызывается ф-я startSlide
         slider.addEventListener('mouseout', (event) => {
@@ -286,115 +286,214 @@ document.addEventListener('DOMContentLoaded', () => {
         const calcBlock = document.querySelector('.calc-block');
         const input = calcBlock.querySelectorAll('input');
         input.forEach((element) => {
-           element.addEventListener('blur', (event) =>{
-               if (event.target.type === 'text') {
-                   event.target.value = event.target.value.replace(/\D/g, '');
-               }
-           })
+            element.addEventListener('blur', (event) =>{
+                if (event.target.type === 'text') {
+                    event.target.value = event.target.value.replace(/\D/g, '');
+                }
+            })
         });
     };
     checkCalcBlock();
 
     //проверка на Name Email Phone
-    const checkEmailAll = () => {
-        const formName = document.querySelectorAll('.form-name'),
-            form1Email = document.querySelectorAll('.form-email'),
-            form1Phone = document.querySelectorAll('.form-phone');
-
-        // const reg = new RegExp(/[78]([-()]*\d){10}/g);
-        // const string = `8(960)-260-20-20`;
-        // const number = reg.test(string);
-        // console.log(number);
-
-        form1Phone.forEach((elem) => {
-            elem.addEventListener('blur', (event) => {
-                if (event.target.type === 'tel'){
-                    const str = event.target.value;
-                    event.target.value = str.match(/(^(?!\+.*\(.*\).*\-\-.*$)(?!\+.*\(.*\).*\-$)(\+[0-9]{1,3}\([0-9]{1,3}\)[0-9]{1}([-0-9]{0,8})?([0-9]{0,1})?)$)|(^[0-9]{1,4}$)/);
-                }
-            })
-        });
-
-        // const string = `bA~t*i-sh_naz!i'k@gmail.com`;
-        // const email = string.match(/[Aa-zZ]\w+~\w+\*\w+-\w+\w+_\w+!\w+'\w+@\w+\.\w{2,4}/g);
-        // console.log(email);
-
-        form1Email.forEach((elem) => {
-           elem.addEventListener('blur', (event) => {
-               if (event.target.type === 'email'){
-                   event.target.value = event.target.value.replace(/[Aa-zZ]\w+~\w+\*\w+-\w+\w+_\w+!\w+'\w+@\w+\.\w{2,4}/g, '');
-               }
-           })
-        });
-
-        formName.forEach((elem) => {
-            elem.addEventListener('blur', (event) => {
-                if (event.target.type === 'text'){
-                    const reg = event.target.value.replace(/(^[A-Z]{1}[a-z]{1,14}[A-Z]{1}[a-z]{1,14}$)|(^[А-Я]{1}[а-я]{1,14} [А-Я]{1}[а-я]{1,14}$)/g, '');
-                    event.target.value = reg;
-                }
-            })
-        });
-
-
-    };
-    checkEmailAll();
+    // const checkEmailAll = () => {
+    //     const formName = document.querySelectorAll('.form-name'),
+    //         form1Email = document.querySelectorAll('.form-email'),
+    //         form1Phone = document.querySelectorAll('.form-phone');
+    //
+    //     // const reg = new RegExp(/[78]([-()]*\d){10}/g);
+    //     // const string = `8(960)-260-20-20`;
+    //     // const number = reg.test(string);
+    //     // console.log(number);
+    //
+    //     form1Phone.forEach((elem) => {
+    //         elem.addEventListener('blur', (event) => {
+    //             if (event.target.type === 'tel'){
+    //                 const str = event.target.value;
+    //                 event.target.value = str.match(/(^(?!\+.*\(.*\).*\-\-.*$)(?!\+.*\(.*\).*\-$)(\+[0-9]{1,3}\([0-9]{1,3}\)[0-9]{1}([-0-9]{0,8})?([0-9]{0,1})?)$)|(^[0-9]{1,4}$)/);
+    //             }
+    //         })
+    //     });
+    //
+    //     // const string = `bA~t*i-sh_naz!i'k@gmail.com`;
+    //     // const email = string.match(/[Aa-zZ]\w+~\w+\*\w+-\w+\w+_\w+!\w+'\w+@\w+\.\w{2,4}/g);
+    //     // console.log(email);
+    //
+    //     form1Email.forEach((elem) => {
+    //        elem.addEventListener('blur', (event) => {
+    //            if (event.target.type === 'email'){
+    //                event.target.value = event.target.value.replace(/[Aa-zZ]\w+~\w+\*\w+-\w+\w+_\w+!\w+'\w+@\w+\.\w{2,4}/g, '');
+    //            }
+    //        })
+    //     });
+    //
+    //     formName.forEach((elem) => {
+    //         elem.addEventListener('blur', (event) => {
+    //             if (event.target.type === 'text'){
+    //                 const reg = event.target.value.replace(/(^[A-Z]{1}[a-z]{1,14}[A-Z]{1}[a-z]{1,14}$)|(^[А-Я]{1}[а-я]{1,14} [А-Я]{1}[а-я]{1,14}$)/g, '');
+    //                 event.target.value = reg;
+    //             }
+    //         })
+    //     });
+    //
+    //
+    // };
+    // checkEmailAll();
 
     //калькулятор
-
     const calc = (price = 100) => {
-      const calcBlock = document.querySelector('.calc-block'),
-          calcType = document.querySelector('.calc-type'),
-          calcSquare = document.querySelector('.calc-square'),
-          calcDay = document.querySelector('.calc-day'),
-          calcCount = document.querySelector('.calc-count'),
-          totalValue = document.getElementById('total');
+        const calcBlock = document.querySelector('.calc-block'),
+            calcType = document.querySelector('.calc-type'),
+            calcSquare = document.querySelector('.calc-square'),
+            calcDay = document.querySelector('.calc-day'),
+            calcCount = document.querySelector('.calc-count'),
+            totalValue = document.getElementById('total');
 
-      const countSum = () => {
-          let total = 0,
-            countValue = 1,
-            dayValue = 1;
-          let step = 50;
-          const typeValue = calcType.options[calcType.selectedIndex].value,
-              squareValue = +calcSquare.value;
+        const countSum = () => {
+            let total = 0,
+                countValue = 1,
+                dayValue = 1;
+            let step = 50;
+            const typeValue = calcType.options[calcType.selectedIndex].value,
+                squareValue = +calcSquare.value;
 
-          if (calcCount.value > 1){
-              countValue += (calcCount.value - 1) / 10;
-          }
-          if (calcDay.value && calcDay.value < 5){
-              dayValue *= 2;
-          } else if (calcDay.value && calcDay.value < 10){
-              dayValue *= 1.5;
-          }
+            if (calcCount.value > 1){
+                countValue += (calcCount.value - 1) / 10;
+            }
+            if (calcDay.value && calcDay.value < 5){
+                dayValue *= 2;
+            } else if (calcDay.value && calcDay.value < 10){
+                dayValue *= 1.5;
+            }
 
-          if (typeValue && squareValue){
-              total = price * typeValue * squareValue * countValue * dayValue;
-          }
+            if (typeValue && squareValue){
+                total = price * typeValue * squareValue * countValue * dayValue;
+            }
 
-          if (totalValue.textContent != total) {
-              if (totalValue.textContent > total) {
-                  step = -1;
-              }
+            if (totalValue.textContent != total) {
+                if (totalValue.textContent > total) {
+                    step = -1;
+                }
 
-              let timer = setInterval(() => {
-                  totalValue.textContent = +totalValue.textContent + step;
-                  if ((total - totalValue.textContent) * step < 1) {
-                      clearInterval(timer);
-                      totalValue.textContent = Math.round(total);
-                  }
-              }, 0);
-          }
-      };
+                let timer = setInterval(() => {
+                    totalValue.textContent = +totalValue.textContent + step;
+                    if ((total - totalValue.textContent) * step < 1) {
+                        clearInterval(timer);
+                        totalValue.textContent = Math.round(total);
+                    }
+                }, 0);
+            }
+        };
 
-      calcBlock.addEventListener('change', (event) => {
-         const target = event.target;
+        calcBlock.addEventListener('change', (event) => {
+            const target = event.target;
 
-         if (target.matches('select') || target.matches('input')){
-             countSum();
-         }
-      });
+            if (target.matches('select') || target.matches('input')){
+                countSum();
+            }
+        });
 
     };
-
     calc(100);
+
+    //send-ajax-form
+    const sendForm = () => {
+        const errorMessage = ' Что-то пошло не так...',
+            loadMessage = ' Загрузка...',
+            successMessage = ' Спасибо! Мы скоро с вами свяжемся!',
+            errorImg = './images/wait/error.png',
+            loadImg = './images/wait/wait.gif',
+            successImg = './images/wait/success.png';
+
+        const postData = (body, outputData, errorData) => {
+            const request = new XMLHttpRequest();
+
+            request.addEventListener('readystatechange', () => {
+                if (request.readyState !== 4) {
+                    return;
+                }
+
+                if (request.status === 200) {
+                    outputData();
+                } else {
+                    errorData(request.status);
+                }
+            });
+
+            request.open('POST', './server.php');
+            request.setRequestHeader('Content-Type', 'application/json');
+            request.send(JSON.stringify(body));
+        };
+
+        const clearInput = idForm => {
+            const form = document.getElementById(idForm);
+
+            [...form.elements]
+                .filter(item =>
+                    item.tagName.toLowerCase() !== 'button' &&
+                    item.type !== 'button')
+                .forEach(item =>
+                    item.value = '');
+        };
+
+        const isValid = event => {
+            const target = event.target;
+
+            if (target.matches('.form-phone')) {
+                target.value = target.value.replace(/[^+\d]/g, '');
+            }
+
+            if (target.name === 'user_name') {
+                target.value = target.value.replace(/[^а-яё ]/gi, '');
+            }
+
+            if (target.matches('.mess')) {
+                target.value = target.value.replace(/[^а-яё ,.]/gi, '');
+            }
+        };
+
+        const processingForm = idForm => {
+            const form = document.getElementById(idForm);
+            const statusMessage = document.createElement('div');
+            const img = document.createElement('img');
+
+            statusMessage.style.cssText = 'font-size: 2rem; color: #fff';
+            img.height = 50;
+
+            form.addEventListener('submit', event => {
+                const formData = new FormData(form);
+                const body = {};
+
+                event.preventDefault();
+                statusMessage.textContent = loadMessage;
+                img.src = loadImg;
+                statusMessage.insertBefore(img, statusMessage.firstChild);
+                form.appendChild(statusMessage);
+
+                formData.forEach((val, key) => {
+                    body[key] = val;
+                });
+
+                postData(body, () => {
+                    statusMessage.textContent = successMessage;
+                    img.src = successImg;
+                    statusMessage.insertBefore(img, statusMessage.firstChild);
+                    clearInput(idForm);
+                }, error => {
+                    statusMessage.textContent = errorMessage;
+                    img.src = errorImg;
+                    statusMessage.insertBefore(img, statusMessage.firstChild);
+                    console.error(error);
+                });
+            });
+            form.addEventListener('input', isValid);
+        };
+
+        processingForm('form1');
+        processingForm('form2');
+        processingForm('form3');
+    };
+
+    sendForm()
+
 });
